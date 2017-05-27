66311723ec60422669dd49ed3c4a2702375dcf58
内藤秀彦 <hidehiko@naitouhidehikonoMacBook-Pro.local>
2017-05-27 22:53:56 +0900

    add controller of get_jan.py.

---
+++ controller/get_jan.py
@@ -0,0 +1,31 @@
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+
+def get_jan(key):
+    url = 'http://askillers.com/jan/?index=1&word=' + urllib.parse.quote(key)
+    req = urllib.request.Request(url)
+    response = urllib.request.urlopen(req)
+    html = response.read()
+    soup = BeautifulSoup(html, "lxml")
+
+    items = soup.find_all("div", class_="item")
+
+    item_list = []
+    for item in items:
+        try:
+            item_texts = item.find_all("div")[-1].text
+            name = item_texts.split("\n\t")[1]
+            jan = item_texts.split("\n\t")[2].replace("JAN/EAN:", "")
+
+            try:
+                image = item.img.attrs["src"]
+            except:
+                image = ""
+
+            item_list += [dict(name=name, jan=jan, image=image)]
+        except:
+            pass
+
+    return item_list
